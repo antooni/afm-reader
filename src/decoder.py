e0f8666643345e7264cc24052ecdba4569fac815
antooni <antoni.pawlak@pm.me>
2022-09-08 22:22:55 +0200

    functionality from google collab with config

--- src/decoder.py
+++ src/decoder.py
@@ -1,22 +1,86 @@
-# make it standalone script which takes parameters from command line
+from distutils.command.config import config
+import string
+from NSFopen.read import read as afmreader
+import matplotlib.pyplot as plt
+import numpy as np
 
-def add(a,b):
-    return a+b
+def decodeAFM(path):
+    return afmreader(path, verbose=False).data
 
-def decode(filename, label, columns, shouldBeTransposed, multiplier, outputFolder, outputName):
-    print("not yet implemented")
+class Config:
+  dirpath = "/mydrive/"
+  resultPath = "/home/private/repos/afm-reader/results/"
+  dataConfig = {
+      "bias": [('Spec', 'Backward', 'Sample_Bias'), ('Spec', 'Forward', 'Sample_Bias')],
+      "amplitude": [('Spec', 'Backward', '2nd Lock-In Amplitude'), ('Spec', 'Forward', '2nd Lock-In Amplitude')],
+      "phase": [('Spec', 'Backward', '2nd Lock-In Phase'), ('Spec', 'Forward', '2nd Lock-In Phase')]
+  }
+  files = ["bias", "amplitude", "phase"]
+  plots = [("bias", "-", "amplitude", "pN"), ("bias", "=", "phase", "deg")]
+config = Config()
+#def checkConfig
 
-    # encode the data with afm reader
 
-    # get the values corresponding to label
-        #if values does not exist then throw
+def getDataFromDecoded(decoded, dataConfig: dict) -> dict:
+  data = {}
 
-    # if shouldBeTransposed === true then transpose
+  for key, value in dataConfig.items():
+    arr = None
+    i = 0
+    for keys in value:
+      try: 
+        d = decoded[keys[0]][keys[1]][keys[2]]
+        if i == 0:
+          arr = d
+        else:
+          arr = np.concatenate((arr, d), axis=1)
+      except: 
+        raise NameError('!!! Config error !!!')
+      i += 1
+    data[key] = arr  
+  return data
 
-    # multiply by multiplier
+def saveFile(name: string, data: dict, dataName: dict):
+  savePath = config.resultPath+name.split('.')[0]+'-'+dataName+'.csv'
+  np.savetxt(savePath, data, fmt='%s', delimiter=",")
 
-    # choose how many columns
+def plot(name, x, y, nameX, unitX, nameY, unitY, xlim = None, ylim = None):
+  for i in range(len(y)):
+      plt.plot(x[i], y[i])
 
-    # save .csv to outputFolder/outputName
+  if xlim != None:
+      plt.xlim(xlim)
+  if ylim != None:
+      plt.ylim(ylim)
+
+  plt.xlabel(nameX + ' ['+ unitX +']')
+  plt.ylabel(nameY + ' ['+ unitY +']')
+
+  savePath = config.resultPath+name.split('.')[0]+'-plot-'+nameX+'-'+ nameY+'.png'
+
+  fig = plt.gcf()
+  fig.savefig(savePath, dpi=300)
+  plt.show()
+
+file = '/home/private/repos/afm-reader/sample/sample.nid'
+fileName = 'sample.nid'
+
+def run():
+  decoded = decodeAFM('/home/private/repos/afm-reader/sample/sample.nid')
+  data = getDataFromDecoded(decoded, config.dataConfig)
+
+  for f in config.files:
+    try:
+      saveFile(fileName, data[f], f)
+    except:
+      raise NameError('!!! Config error !!!')
+
+  for p in config.plots:
+    try:
+      plot(fileName,data[p[0]], data[p[2]], p[0], p[1], p[2], p[3])
+    except:
+      raise NameError('!!! Config error !!!')
+
+run()
 
 
--- src/test_decoder.py
+++ src/test_decoder.py
@@ -1,4 +1,44 @@
 import decoder
+import numpy as np
+import pytest
 
-def testA():
-    assert decoder.add(1,1) == 2
+
+def decodesCorrectly():
+    data = decoder.decodeAFM('/home/private/repos/afm-reader/sample/sample.nid')
+    assert len(data) == 32
+
+def testOptimistic():
+    decoded = {
+        "Spec": {
+            "Backward": {
+                "Sample_Bias": [[1,2,3]],
+                "2nd Lock-In Amplitude": [[6,5,4]]
+            },
+            "Forward": {
+                "Sample_Bias": [[4,5,6]],
+                "2nd Lock-In Amplitude": [[3,2,1]]
+            }
+        }
+    }
+    dataConfig = {
+      "x": [('Spec', 'Backward', 'Sample_Bias'), ('Spec', 'Forward', 'Sample_Bias')],
+      "amplitude": [('Spec', 'Backward', '2nd Lock-In Amplitude'), ('Spec', 'Forward', '2nd Lock-In Amplitude')],
+    }
+    result = decoder.getDataFromDecoded(decoded, dataConfig)
+    assert np.array_equal(result["x"], np.array([[1,2,3,4,5,6]]))
+    assert np.array_equal(result["amplitude"], np.array([[6,5,4,3,2,1]]))
+
+def testPessimistic():
+    decoded = {}
+    dataConfig = {
+      "x": [('Spec', 'Backward', 'Sample_Bias'), ('Spec', 'Forward', 'Sample_Bias')],
+    }
+
+    with pytest.raises(Exception) as exception:
+        decoder.getDataFromDecoded(decoded, dataConfig)
+    assert str(exception.value) == '!!! Config error !!!' 
+
+
+
+
+
