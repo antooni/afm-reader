fd27d395d68938d0ae9be24c4b630c1cdc2a6295
antooni <antoni.pawlak@pm.me>
2022-10-11 22:47:30 +0200

    change filename to key

--- src/file_generator.py
+++ src/file_generator.py
@@ -6,14 +6,14 @@ import numpy as np
 from typing import Dict
 
 def file_all(config: Dict[str, FileConfig], data, iowrapper: IOWrapper):
-    for _, file in config.items():
+    for key, file in config.items():
         try:
-            save_file(data[file.data_key], file, iowrapper)
+            save_file(data[file.data_key], file, key, iowrapper)
         except:
             raise NameError('!!! Config error !!!')
 
-def save_file(data: np.ndarray, file_config: FileConfig, iowrapper: IOWrapper):
-    path = os.path.join(iowrapper.get_file_path(file_config.data_key), file_config.data_key)
+def save_file(data: np.ndarray, file_config: FileConfig, file_name: str, iowrapper: IOWrapper):
+    path = os.path.join(iowrapper.get_file_path(file_name), file_name)
 
     if file_config.multiplier != 1:
         multiplied = np.multiply(data, file_config.multiplier)
